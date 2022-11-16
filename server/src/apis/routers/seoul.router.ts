b39b88e8d59e346b4b5b06a4dabb776e48854a5e
Yunu <gyunu47@gmail.com>
2022-11-16 20:40:48 +0900

    [#4] feat : 서울 도시데이터 테스트 api

--- server/src/apis/routers/seoul.router.ts
+++ server/src/apis/routers/seoul.router.ts
@@ -1,9 +1,40 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const router = express.Router();
 
-router.get('/', (_: Request, res: Response) => {
-  res.send({ test: 'hi' });
+router.get('/', async (_: Request, res: Response) => {
+  const url = `http://openapi.seoul.go.kr:8088/${process.env.SEOUL_CITY_API_ACCESS_KEY}/xml/citydata/1/5/광화문·덕수궁`;
+
+  try {
+    const { data } = await axios({
+      url: url,
+      method: 'get'
+    });
+
+    res.send({ message: '성공', data });
+  } catch (err) {
+    res.send({ message: '실패' });
+  }
 });
-router.get('/:placeId', () => {});
+
+router.get('/:areaName', async (req: Request, res: Response) => {
+  const { areaName } = req.params;
+  const url = `http://openapi.seoul.go.kr:8088/${process.env.SEOUL_CITY_API_ACCESS_KEY}/xml/citydata/1/5/${areaName}`;
+
+  try {
+    const { data } = await axios({
+      url: url,
+      method: 'get'
+    });
+
+    res.send({ message: '성공', data });
+  } catch (err) {
+    res.send({ message: '실패' });
+  }
+});
 
 export default router;
