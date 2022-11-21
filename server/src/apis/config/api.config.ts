4bad447f1f32081b1f5ec535f4d4e54b05149e5b
HyunJungJo98 <kchwoonn0827@naver.com>
2022-11-21 15:31:17 +0900

    [#7] refactor : xml로 받아오는 부분, json 파싱하는 부분을 각각 비동기로 분리

--- server/src/apis/config/api.config.ts
+++ server/src/apis/config/api.config.ts
@@ -2,4 +2,4 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const SEOUL_CITY_API_BASE_URL = `http://openapi.seoul.go.kr:8088/${process.env.SEOUL_CITY_API_ACCESS_KEY}/xml/citydata/1/5/`;
+export const SEOUL_CITY_API_BASE_URL = `http://openapi.seoul.go.kr:8088/${process.env.SEOUL_CITY_API_ACCESS_KEY_SUB}/xml/citydata/1/5/`;
--- server/src/apis/controllers/seoul.controller.ts
+++ server/src/apis/controllers/seoul.controller.ts
@@ -1,50 +1,47 @@
-import { AREA_NAMES } from './../config/area.config';
-// import { TEST_AREA_NAMES } from '../config/area.config';
+// import { AREA_NAMES } from './../config/area.config';
+import { TEST_AREA_NAMES } from '../config/area.config';
 import { Request, Response } from 'express';
 // import { AREA_NAMES } from '../config/area.config';
 import xml2js from 'xml2js';
 import { getAxiosSeoulArea } from '../utils/axios';
-import { cityDataTypes } from '../types/interfaces';
 
 export default {
   allAreas: async (_: Request, res: Response) => {
     try {
-      const cityData: cityDataTypes[] = await Promise.all(
-        Object.keys(AREA_NAMES).map(async areaName => {
-          const data = await getAxiosSeoulArea(areaName);
-          const json = await xml2js.parseStringPromise(data);
-
-          if (json['Map']) {
-            console.log(`실패: ${areaName}`);
-            return {
-              areaName: null,
-              populationMin: null,
-              populationMax: null,
-              populationLevel: null,
-              updatedAt: null
-            };
-          }
-          console.log(`성공: ${areaName}`);
-          const { AREA_NM } = json['SeoulRtd.citydata']['CITYDATA'][0];
-          const {
-            AREA_PPLTN_MIN,
-            AREA_PPLTN_MAX,
-            AREA_CONGEST_LVL,
-            PPLTN_TIME
-          } =
-            json['SeoulRtd.citydata']['CITYDATA'][0]['LIVE_PPLTN_STTS'][0][
-              'LIVE_PPLTN_STTS'
-            ][0];
-
-          return {
-            areaName: AREA_NM[0],
-            populationMin: AREA_PPLTN_MIN[0],
-            populationMax: AREA_PPLTN_MAX[0],
-            populationLevel: AREA_CONGEST_LVL[0],
-            updatedAt: PPLTN_TIME[0]
-          };
-        })
+      const cityDataXml = await Promise.all(
+        Object.keys(TEST_AREA_NAMES).map(async areaName =>
+          getAxiosSeoulArea(areaName)
+        )
       );
+
+      const cityDataJson = await Promise.all(
+        cityDataXml.map(async xml => xml2js.parseStringPromise(xml))
+      );
+
+      const cityData = cityDataJson.map(json => {
+        if (json['Map']) {
+          return {
+            areaName: null,
+            populationMin: null,
+            populationMax: null,
+            populationLevel: null,
+            updatedAt: null
+          };
+        }
+        const { AREA_NM } = json['SeoulRtd.citydata']['CITYDATA'][0];
+        const { AREA_PPLTN_MIN, AREA_PPLTN_MAX, AREA_CONGEST_LVL, PPLTN_TIME } =
+          json['SeoulRtd.citydata']['CITYDATA'][0]['LIVE_PPLTN_STTS'][0][
+            'LIVE_PPLTN_STTS'
+          ][0];
+
+        return {
+          areaName: AREA_NM[0],
+          populationMin: AREA_PPLTN_MIN[0],
+          populationMax: AREA_PPLTN_MAX[0],
+          populationLevel: AREA_CONGEST_LVL[0],
+          updatedAt: PPLTN_TIME[0]
+        };
+      });
       res.status(200).json({ ok: true, data: cityData });
     } catch (error) {
       console.log(error);
