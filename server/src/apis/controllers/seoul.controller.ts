8d3aa615fddd857e07335ada1609c9e38f470862
HyunJungJo98 <kchwoonn0827@naver.com>
2022-11-21 23:52:51 +0900

    [#9] feature : 인구 밀도 데이터 저장 확인

--- server/src/apis/controllers/seoul.controller.ts
+++ server/src/apis/controllers/seoul.controller.ts
@@ -1,47 +1,15 @@
 // import { AREA_NAMES } from './../config/area.config';
-import { TEST_AREA_NAMES } from '../config/area.config';
 import { Request, Response } from 'express';
-// import { AREA_NAMES } from '../config/area.config';
-import xml2js from 'xml2js';
-import { getAxiosSeoulArea } from '../utils/axios';
+import seoulService from '../services/seoul.service';
 
 export default {
   allAreas: async (_: Request, res: Response) => {
     try {
-      const cityDataXml = await Promise.all(
-        Object.keys(TEST_AREA_NAMES).map(async areaName =>
-          getAxiosSeoulArea(areaName)
-        )
-      );
+      const cityData = await seoulService.getCityData();
+      console.log(cityData);
 
-      const cityDataJson = await Promise.all(
-        cityDataXml.map(async xml => xml2js.parseStringPromise(xml))
-      );
+      seoulService.savePopulationData(cityData);
 
-      const cityData = cityDataJson.map(json => {
-        if (json['Map']) {
-          return {
-            areaName: null,
-            populationMin: null,
-            populationMax: null,
-            populationLevel: null,
-            populationTime: null
-          };
-        }
-        const { AREA_NM } = json['SeoulRtd.citydata']['CITYDATA'][0];
-        const { AREA_PPLTN_MIN, AREA_PPLTN_MAX, AREA_CONGEST_LVL, PPLTN_TIME } =
-          json['SeoulRtd.citydata']['CITYDATA'][0]['LIVE_PPLTN_STTS'][0][
-            'LIVE_PPLTN_STTS'
-          ][0];
-
-        return {
-          areaName: AREA_NM[0],
-          populationMin: AREA_PPLTN_MIN[0],
-          populationMax: AREA_PPLTN_MAX[0],
-          populationLevel: AREA_CONGEST_LVL[0],
-          populationTime: PPLTN_TIME[0]
-        };
-      });
       res.status(200).json({ ok: true, data: cityData });
     } catch (error) {
       console.log(error);
