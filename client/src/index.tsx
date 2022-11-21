fc948ad7b3c12ad049c40765a8ac4d67029d30da
bullmouse <sjpark960520@gmail.com>
2022-11-21 13:51:17 +0900

    [#5] setting : CRA에서 불필요한 파일 정리

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(container!);
+
+root.render(<App />);
