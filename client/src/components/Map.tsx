caf995f6f38e711b7b8a2f667651632e3a372919
bullmouse <sjpark960520@gmail.com>
2022-11-21 21:29:01 +0900

    [#11] feat : weatherapi를 사용해서 Reverse Geocoding 구현

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import useGeolocation from 'react-hook-geolocation';
+import axios from 'axios';
 
 const MapComponent = styled.div`
   width: 100%;
@@ -9,17 +10,20 @@ const MapComponent = styled.div`
 
 const Map = () => {
   const mapRef = useRef(null);
-
   const [coordinates, setCoordinates] = useState({
-    latitude: 37.5656,
-    longitude: 126.9769
+    latitude: 0,
+    longitude: 0
   });
-
   const geolocation = useGeolocation({
     enableHighAccuracy: true,
     maximumAge: 15000,
     timeout: 12000
   });
+
+  const defaultCoords = {
+    latitude: 37.5656,
+    longitude: 126.9769
+  };
 
   useEffect(() => {
     const { naver } = window;
@@ -34,11 +38,26 @@ const Map = () => {
 
     if (!mapRef.current || !naver) return;
 
+    const fetchCoordsDataFromAPI = async () => {
+      // appid의 값은 나중에 환경변수로 따로 빼내야 한다.
+      const apiURL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${geolocation.latitude}&lon=${geolocation.longitude}&limit=5&appid=7bb7527a2dbfc2c2bf671f2bc4dc5fc1`;
+      const responseData = await axios.get(apiURL);
+      const cityData = await responseData.data[0]?.local_names.en;
+
+      // 'Osan-si', 'Seoul'
+      if (cityData && cityData !== 'Seoul') {
+        setCoordinates(defaultCoords);
+      }
+      if (cityData && cityData === 'Seoul') {
+        setCoordinates({
+          latitude: geolocation.latitude,
+          longitude: geolocation.longitude
+        });
+      }
+    };
+
     if (geolocation.latitude && geolocation.longitude) {
-      setCoordinates({
-        latitude: geolocation.latitude,
-        longitude: geolocation.longitude
-      });
+      fetchCoordsDataFromAPI();
     }
 
     (() => new naver.maps.Map(mapRef.current, mapOptions))();
