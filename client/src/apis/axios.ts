97b5b6718bd30eb5cec3f0f4575663494e7ef128
picoloman6 <a2328b@naver.com>
2022-11-22 17:35:35 +0900

    [#14] fix : cors 에러 해결

--- client/src/apis/axios.ts
+++ client/src/apis/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 // 나중에 .env로 빼내어야 함
 const serverURL = 'http://localhost:3001';
 
@@ -16,7 +17,5 @@ const fetchGeocodeFromCoords = async (lat: number, lng: number) => {
 
   return data?.data.results[0].region.area1.name;
 };
-
-// https://ifh.cc/g/RQcSZX.gif
 
 export default fetchGeocodeFromCoords;
