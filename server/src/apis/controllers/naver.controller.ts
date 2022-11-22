bd1fa5c4b1de6c6dd68df9d185a7b167a9d80750
picoloman6 <a2328b@naver.com>
2022-11-22 12:04:24 +0900

    [#11] fix : 네이버 지도 cors 에러 해결

--- server/src/apis/controllers/naver.controller.ts
+++ server/src/apis/controllers/naver.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 import { getAxiosFromNaverApi } from '../utils/axios';
+
+dotenv.config();
 
 export default {
   getGeoCodingFromCoords: async (req: Request, res: Response) => {
@@ -8,6 +11,9 @@ export default {
     const url = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${lng},${lat}&sourcecrs=epsg:4326&orders=roadaddr&output=json`;
     try {
       const data = await getAxiosFromNaverApi(url);
+      if (process.env.CLIENT_URL) {
+        res.setHeader('Access-Control-Allow-origin', process.env.CLIENT_URL);
+      }
       res.status(200).send(data);
     } catch (error) {
       console.log(error);
