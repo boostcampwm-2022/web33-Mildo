17ad42ea5908f9da06582eadac62a35378e077fe
picoloman6 <a2328b@naver.com>
2022-11-22 15:25:46 +0900

    [#14] refactor : 위치 관련 로직 Map에서 MainPage로 이동 및 로딩 페이지 리팩토링

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,38 +1,20 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import useGeolocation from 'react-hook-geolocation';
-import MapLoading from './MapLoading';
-
-import fetchGeocodeFromCoords from '../apis/axios';
+import { MapComponentPropsType } from '../types/interfaces';
 
 const MapComponent = styled.div`
   width: 100%;
   height: 100%;
 `;
 
-const Map = () => {
+const Map = ({ latitude, longitude }: MapComponentPropsType) => {
   const mapRef = useRef(null);
-  // 기본 좌표를 시청역으로 설정
-  const defaultCoords = {
-    latitude: 37.5656,
-    longitude: 126.9769
-  };
-
-  const [coordinates, setCoordinates] = useState({ latitude: 0, longitude: 0 });
-  const [isLoading, setIsLoading] = useState(true);
-
-  const geolocation = useGeolocation({
-    enableHighAccuracy: true,
-    maximumAge: 15000,
-    timeout: 12000
-  });
 
   useEffect(() => {
     const { naver } = window;
-    const location = new naver.maps.LatLng(
-      coordinates.latitude,
-      coordinates.longitude
-    );
+
+    const location = new naver.maps.LatLng(latitude, longitude);
+
     const mapOptions: naver.maps.MapOptions = {
       center: location,
       zoom: 16,
@@ -44,35 +26,14 @@ const Map = () => {
     };
 
     if (!mapRef.current || !naver) {
-      console.log('hello!');
       return;
-    }
-
-    // 위도, 경도가 있는 경우 네이버 API에 주소 요청
-    if (geolocation.latitude && geolocation.longitude) {
-      fetchGeocodeFromCoords(geolocation.latitude, geolocation.longitude).then(
-        result => {
-          // 서울인 경우 -> 해당 위치를 결과 값으로 리턴
-          // 서울이 아닌 경우 -> 서울 중심 위치를 결과값으로 리턴
-          setIsLoading(false);
-          if (result === '서울특별시' || result === '과천시') {
-            setCoordinates({
-              latitude: geolocation.latitude,
-              longitude: geolocation.longitude
-            });
-          }
-          if (result !== '서울특별시') {
-            setCoordinates({ ...defaultCoords });
-          }
-        }
-      );
     }
 
     // MapComponent DOM에 네이버 지도 렌더링
     (() => new naver.maps.Map(mapRef.current, mapOptions))();
-  }, [geolocation]);
+  }, []);
 
-  return isLoading ? <MapLoading /> : <MapComponent ref={mapRef} />;
+  return <MapComponent ref={mapRef} />;
 };
 
 export default Map;
