94f67b5fd8283ebe6c99c3e0efc1ac071743afbd
picoloman6 <a2328b@naver.com>
2022-11-22 11:49:33 +0900

    [#11] feat : FE에서 BE에 네이버 지도 GeoCoding 요청 구현

--- client/src/apis/axios.ts
+++ client/src/apis/axios.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
-export const fetchGeocodeFromCoords = async () => {
-  const data = await axios.get('/api/naver');
+// 나중에 .env로 빼내어야 함
+const serverURL = 'http:/localhost:3001';
+
+const fetchGeocodeFromCoords = async (lat: number, lng: number) => {
+  try {
+    const data = await axios.get(
+      `${serverURL}/api/naver?lng=${lng}&lat=${lat}`
+    );
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+export default fetchGeocodeFromCoords;
