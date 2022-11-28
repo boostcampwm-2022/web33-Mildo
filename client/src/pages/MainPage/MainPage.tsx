04cdb7762f75272a01718fd1517c8a5d26d568d1
Yunu <gyunu47@gmail.com>
2022-11-28 16:16:40 +0900

    [#31] fix : 로딩 화면 렌더링 및 사용자 위치 정보 거부시 서울 중심지 보여주기

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,17 +1,13 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
-import useGeolocation from 'react-hook-geolocation';
-
 import Map from '../../components/Map/Map';
 import MapLoading from '../../components/MapLoading/MapLoading';
 // import fetchGeocodeFromCoords from '../../apis/axios';
 import InfoDetailModal from '../../components/InfoDetailModal/InfoDetailModal';
-import {
-  DEFAULT_COORDINATES,
-  GEOLOCATION_CONSTANTS,
-  USERS_LOCATION
-} from '../../config/constants';
+import { DEFAULT_COORDINATES, USERS_LOCATION } from '../../config/constants';
 import apis from '../../apis/apis';
+
+// GEOLOCATION_CONSTANTS
 
 const StyledMainPage = styled.div`
   width: 100vw;
@@ -34,17 +30,14 @@ interface UsersLocationResponseTypes {
 }
 
 const MainPage = () => {
-  const [coordinates, setCoordinates] = useState<CoordinatesTypes>({
-    latitude: 0,
-    longitude: 0
-  });
+  const [coordinates, setCoordinates] = useState<CoordinatesTypes | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const geolocation = useGeolocation({
-    enableHighAccuracy: true,
-    maximumAge: GEOLOCATION_CONSTANTS.MAXIMUMAGE,
-    timeout: GEOLOCATION_CONSTANTS.TIMEOUT
-  });
+  // const geolocation = useGeolocation({
+  //   enableHighAccuracy: true,
+  //   maximumAge: GEOLOCATION_CONSTANTS.MAXIMUMAGE,
+  //   timeout: GEOLOCATION_CONSTANTS.TIMEOUT
+  // });
 
   const isUserInSeoulOrGwaCheon = (usersLocation: string) => {
     return (
@@ -53,35 +46,51 @@ const MainPage = () => {
     );
   };
 
-  const setMapCenter = async () => {
-    if (!geolocation.latitude || !geolocation.longitude) {
-      setIsLoading(false);
-      setCoordinates({ ...DEFAULT_COORDINATES });
-      return;
-    }
+  const setMapCenter = async (latitude: number, longitude: number) => {
     // 위도, 경도가 있는 경우 네이버 API에 주소 요청
     const usersLocationResponse: UsersLocationResponseTypes =
-      await apis.getUsersLocation(geolocation.latitude, geolocation.longitude);
+      await apis.getUsersLocation(latitude, longitude);
 
-    setIsLoading(false);
     if (!usersLocationResponse.results) {
       return;
     }
     const userLocation = usersLocationResponse.results[0].region.area1.name;
 
     if (isUserInSeoulOrGwaCheon(userLocation)) {
-      setCoordinates({
-        latitude: geolocation.latitude,
-        longitude: geolocation.longitude
-      });
+      setCoordinates({ latitude, longitude });
       return;
     }
     setCoordinates({ ...DEFAULT_COORDINATES });
   };
 
   useEffect(() => {
-    setMapCenter();
-  }, [geolocation]);
+    // 사용자 위치 정보 알아내기
+    const success = (geolocationPosition: GeolocationPosition) => {
+      // 위도/경도 : geolocationPosition.coords.longitude, geolocationPosition.coords.latitude,
+      console.log('위치 정보 허용: ', geolocationPosition);
+      setMapCenter(
+        geolocationPosition.coords.latitude,
+        geolocationPosition.coords.longitude
+      );
+    };
+
+    const error = (value: GeolocationPositionError) => {
+      console.log('위치 정보 거부');
+      console.log(value.code, value.message);
+      setCoordinates({ ...DEFAULT_COORDINATES });
+    };
+
+    navigator.geolocation.watchPosition(success, error);
+  }, []);
+
+  useEffect(() => {
+    if (!coordinates) {
+      return;
+    }
+    // 서울 중심 여부 혹은 사용자 위치 정보 허용 여부에 따라 다른 초기 위치 랜더링
+    console.log(coordinates);
+    setIsLoading(false);
+  }, [coordinates]);
 
   return (
     <StyledMainPage>
@@ -89,8 +98,8 @@ const MainPage = () => {
         <MapLoading />
       ) : (
         <Map
-          latitude={coordinates.latitude}
-          longitude={coordinates.longitude}
+          latitude={coordinates!.latitude}
+          longitude={coordinates!.longitude}
         />
       )}
       <InfoDetailModal />
