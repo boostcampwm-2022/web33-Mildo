6b0c29cfa493776e371d551763e04d7175778ddf
bullmouse <sjpark960520@gmail.com>
2022-11-29 17:00:43 +0900

    [#41] feat : 모달창 내부 UI 스타일링 구현

--- client/src/components/LoginModal/LoginModal.tsx
+++ client/src/components/LoginModal/LoginModal.tsx
@@ -1,9 +1,11 @@
 import { useAtom } from 'jotai';
 import styled from 'styled-components';
+import { IoClose } from 'react-icons/io5';
 
 import Modal from '../Modal/Modal';
 import { isLoginModalOpenAtom } from '../../atom/login';
 import ModalFilter from '../ModalFilter/ModalFilter';
+import { createNaverLoginSvg } from '../../utils/login.util';
 
 const LoginModalLayout = styled.div`
   position: absolute;
@@ -16,9 +18,50 @@ const LoginModalLayout = styled.div`
   min-width: 250px;
   height: 150px;
   background-color: white;
-  display: block;
-
   border-radius: 10px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`;
+
+const TitleBar = styled.div`
+  background-color: #6349ff;
+  display: block;
+  width: 100%;
+  height: 40px;
+  border-radius: 10px 10px 0 0;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  position: relative;
+
+  button {
+    background: none;
+    border: none;
+    padding-left: 7px;
+    size: 20px;
+
+    position: absolute;
+    left: 0;
+    top: 30%;
+  }
+
+  h2 {
+    display: block;
+    padding-top: 6px;
+    margin: 0 auto;
+    font-size: 20px;
+    color: white;
+  }
+`;
+
+const NaverLoginBtn = styled.button`
+  background-color: white;
+  border: none;
 `;
 
 const LoginModal = () => {
@@ -27,7 +70,24 @@ const LoginModal = () => {
   return (
     <Modal isOpen={isLoginModalOpen}>
       <ModalFilter />
-      <LoginModalLayout></LoginModalLayout>{' '}
+      <LoginModalLayout>
+        <TitleBar>
+          <button>
+            <IoClose
+              style={{
+                color: 'white',
+                width: '18px',
+                height: '18px'
+              }}
+            />
+          </button>
+          <h2>로그인</h2>
+        </TitleBar>
+        <NaverLoginBtn
+          dangerouslySetInnerHTML={{
+            __html: createNaverLoginSvg()
+          }}></NaverLoginBtn>
+      </LoginModalLayout>
     </Modal>
   );
 };
--- client/src/utils/map.util.ts
+++ client/src/utils/map.util.ts
@@ -1,7 +1,6 @@
 import { POPULATION_LEVEL_COLOR } from '../config/constants';
 import { MarkerObjectTypes } from '../types/interfaces';
 
-// eslint-disable-next-line import/prefer-default-export
 export const createPinSvg = (populationLevel: string) => {
   return `<svg width="35" height="50" viewBox="0 0 35 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M34 19.181C34 21.5965 32.9892 24.6791 31.3657 28.0295C29.7552 31.3532 27.5979 34.8289 25.4253 37.9975C23.255 41.1627 21.0826 44.0023 19.4519 46.0516C18.6461 47.0642 17.9735 47.8826 17.5 48.4506C17.0265 47.8826 16.3539 47.0642 15.5481 46.0516C13.9174 44.0023 11.745 41.1627 9.57474 37.9975C7.40211 34.8289 5.24479 31.3532 3.63429 28.0295C2.01083 24.6791 1 21.5965 1 19.181C1 9.05163 8.47165 1 17.5 1C26.5283 1 34 9.05163 34 19.181Z" fill="${POPULATION_LEVEL_COLOR[populationLevel].fill}" stroke="${POPULATION_LEVEL_COLOR[populationLevel].stroke}" stroke-width="2"/></svg>`;
 };
