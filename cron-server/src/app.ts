4173f8b236a0a165ffacc5bb931dc0e125399835
bullmouse <sjpark960520@gmail.com>
2022-11-29 10:43:05 +0900

    "[#30] fix : app.ts에 있는 주석 삭제"

--- cron-server/src/app.ts
+++ cron-server/src/app.ts
@@ -8,18 +8,3 @@ connectMongoDB();
 dotenv.config();
 
 cron.schedule(`0,30 * * * *`, cronController.cronSeoulData);
-// cronController.cronSeoulData();
-// import * as redis from 'redis';
-
-// const redisClient = redis.createClient({
-//   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-//   legacyMode: true // 반드시 설정 !!
-// });
-// redisClient.on('connect', () => {
-//   console.info('Redis connected!');
-// });
-// redisClient.on('error', err => {
-//   console.error('Redis Client Error', err);
-// });
-// redisClient.connect().then(); // redis v4 연결 (비동기)
-// const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
