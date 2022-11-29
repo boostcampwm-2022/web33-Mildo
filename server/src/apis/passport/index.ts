05f77c6f5049d3ec71d8b0c64686ee97310f1fe4
bullmouse <sjpark960520@gmail.com>
2022-11-29 23:06:37 +0900

    [#45] fix : deserializeUser 여러 번 호출되는 문제 해결

--- server/src/apis/passport/index.ts
+++ server/src/apis/passport/index.ts
@@ -6,6 +6,8 @@ export default () => {
   // done(null, user.id)로 세션을 초기화 한다.
   passport.serializeUser(
     (user: Express.User, done: (err: any, id?: unknown) => void) => {
+      console.log('최초의 로그인');
+      console.log(user);
       done(null, user);
     }
   );
@@ -17,6 +19,8 @@ export default () => {
       user: Express.User,
       done: (err: any, user?: false | Express.User | null | undefined) => void
     ) => {
+      console.log('이후의 로그인');
+      console.log(user);
       done(null, user);
     }
   );
