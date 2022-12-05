68defc63826175824fc0742998e5d255e1633529
bullmouse <sjpark960520@gmail.com>
2022-12-05 16:18:05 +0900

    [#64] feat : 버튼 비활성화 모드 스타일링 및 설정

--- client/src/components/DensityFilterListBtn/DensityFilterListBtn.tsx
+++ client/src/components/DensityFilterListBtn/DensityFilterListBtn.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface BtnStyleProps {
   bgColor: string;
   borderColor: string;
+  disable: boolean;
 }
 
 interface FilterListBtnProps extends BtnStyleProps {
@@ -14,21 +15,24 @@ const BtnStyle = styled.li<BtnStyleProps>`
   display: block;
   width: 4rem;
   height: 1.5rem;
-  background-color: ${props => props.bgColor || 'blue'};
-  border: 1px solid ${props => props.borderColor || 'white'};
+  box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.3);
+  background-color: ${props => (!props.disable ? props.bgColor : '#BFBFBF')};
+  border: 1px solid ${props => (!props.disable ? props.borderColor : '#999999')};
   border-radius: 200px;
   font-size: 0.8rem;
   text-align: center;
   padding-top: 0.2rem;
 
   color: white;
-  text-shadow: 1px 1px ${props => props.borderColor || 'black'};
+  text-shadow: 1px 1px
+    ${props => (!props.disable ? props.borderColor : '#BFBFBF')};
 
   cursor: pointer;
   user-select: none;
 
   &:active {
-    background-color: ${props => props.borderColor || 'white'};
+    background-color: ${props =>
+      !props.disable ? props.borderColor : '#999999'};
   }
 `;
 
@@ -37,8 +41,18 @@ const DensityFilterListBtn: React.FC<FilterListBtnProps> = ({
   borderColor,
   contents
 }: FilterListBtnProps) => {
+  const [isDeactivate, setIsDeactivate] = useState(false);
+
+  const toggleActivationHandler = () => {
+    setIsDeactivate(prev => !prev);
+  };
+
   return (
-    <BtnStyle bgColor={bgColor} borderColor={borderColor}>
+    <BtnStyle
+      onClick={toggleActivationHandler}
+      bgColor={bgColor}
+      borderColor={borderColor}
+      disable={isDeactivate}>
       {contents}
     </BtnStyle>
   );
