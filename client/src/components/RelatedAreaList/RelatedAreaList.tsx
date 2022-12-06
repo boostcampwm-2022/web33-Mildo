0da260a94bec9fa5c9262ef05b3ff457601357ae
Yunu <gyunu47@gmail.com>
2022-12-06 11:36:54 +0900

    [#72] fix : 주석 제거 및 deferredSearchAreaName 적용

--- client/src/components/RelatedAreaList/RelatedAreaList.tsx
+++ client/src/components/RelatedAreaList/RelatedAreaList.tsx
@@ -17,10 +17,6 @@ const RelatedAreaListStyle = styled.div`
   border-radius: 10px;
   border: none;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
-
-  // ::-webkit-scrollbar {
-  //   display:
-  // }
 `;
 
 interface CoordinatesTypes {
