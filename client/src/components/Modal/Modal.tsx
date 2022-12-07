d7fdb5e53a3624e8191282a1efc3043361296e16
bullmouse <sjpark960520@gmail.com>
2022-12-07 17:53:38 +0900

    [#68] feat : 모달 애니메이션 구현

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,17 +1,49 @@
 import { SetStateAction } from 'react';
-import styled, { CSSProp } from 'styled-components';
+import styled, { CSSProp, keyframes, css } from 'styled-components';
 import { Z_INDEX } from '../../config/constants';
 
 interface ModalContainerProps {
   customModalStyle?: CSSProp;
+  open: boolean;
 }
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+    display: block;
+  } 
+  100% { 
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+    display: block;
+  } 
+  100% { 
+    opacity: 0;
+  }
+`;
 
 const ModalContainer = styled.div<ModalContainerProps>`
   z-index: ${Z_INDEX.MODAL};
   ${props => props.customModalStyle && props.customModalStyle}
+  /* display: ${props => !props.open && 'none'}; */
+  visibility: ${props => (props.open ? 'visible' : 'hidden')};
+  ${props => !props.open && `z-index: -100`};
+  animation: ${props =>
+    props.open
+      ? css`
+          ${fadeIn} .2s linear
+        `
+      : css`
+          ${fadeOut} .2s linear
+        `};
 `;
 
-const Filter = styled.div`
+const Filter = styled.div<{ open: boolean }>`
   position: absolute;
   top: 0;
   left: 0;
@@ -19,7 +51,16 @@ const Filter = styled.div`
   height: 100vh;
   z-index: ${Z_INDEX.FILTER};
   background-color: rgba(0, 0, 0, 0.5);
-  display: block;
+  /* display: ${props => (props.open ? 'block' : 'none')}; */
+  visibility: ${props => (props.open ? 'visible' : 'hidden')};
+  animation: ${props =>
+    props.open
+      ? css`
+          ${fadeIn} .2s linear
+        `
+      : css`
+          ${fadeOut} .2s linear
+        `};
 `;
 
 interface ModalProps {
@@ -47,14 +88,18 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-      {isOpen && (
+      {/* {isOpen && (
         <>
           {background && <Filter onClick={clickModalHandler} />}
           <ModalContainer customModalStyle={customModalStyle}>
             {children}
           </ModalContainer>
         </>
-      )}
+      )} */}
+      <Filter onClick={clickModalHandler} open={isOpen && background} />
+      <ModalContainer customModalStyle={customModalStyle} open={isOpen}>
+        {children}
+      </ModalContainer>
     </>
   );
 };
