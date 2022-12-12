7c23d75301c83315c6cbd5aa329b1143ca332a54
picoloman6 <a2328b@naver.com>
2022-12-12 20:47:13 +0900

    vite 적용 끝

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteCompression()],
   server: {
     open: true,
     port: 3000
