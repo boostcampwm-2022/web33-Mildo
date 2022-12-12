0373fa175f5ff437a12c112d8b91368a3b102b1d
HyunJungJo98 <kchwoonn0827@naver.com>
2022-12-12 16:47:00 +0900

    [#98] test : 필터 렌더링 테스트 코드 작성

--- client/cypress/e2e/filter.cy.js
+++ client/cypress/e2e/filter.cy.js
@@ -1,1 +1,14 @@
-describe('필터 렌더링 테스트', () => {});
+describe('필터 렌더링 테스트', () => {
+  it('필터 렌더링', () => {
+    cy.visit('https://www.mildo.live').wait(2000);
+
+    const populationLevels = ['매우 붐빔', '붐빔', '보통', '여유'];
+
+    // 잘 렌더 되는지 확인
+    populationLevels.map((populationLevel, index) => {
+      cy.get(`ul>li:nth-child(${index + 1})`)
+        .should('be.visible')
+        .contains(populationLevel);
+    });
+  });
+});
--- client/cypress/e2e/login.cy.js
+++ client/cypress/e2e/login.cy.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line no-undef
 describe('mildo 로그인 테스트', () => {
+  // eslint-disable-next-line no-undef
   it('mildo 로그인 기능 구현하기', () => {
     // 1. 웹사이트 접속하기
     cy.visit('https://www.mildo.live').wait(6000);
