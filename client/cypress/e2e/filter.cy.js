254c678b2c01a7d0eb000df92589984a408c7530
Yunu <gyunu47@gmail.com>
2022-12-13 16:28:10 +0900

    [#98] fix : 필터링 테스트

--- client/cypress/e2e/filter.cy.js
+++ client/cypress/e2e/filter.cy.js
@@ -3,42 +3,79 @@ describe('필터 렌더링 테스트', () => {
   const populationColors = ['#FF1E1E', '#FF9900', '#FFDB1D', '#43EB40'];
 
   beforeEach(() => {
-    cy.visit('https://www.mildo.live').wait(2000);
+    cy.visit(Cypress.env('products_url')).wait(2000);
   });
 
   // 잘 렌더 되는지 확인
-  it('필터 렌더링', () => {
+  it.skip('필터 렌더링', () => {
     populationLevels.map((populationLevel, index) => {
       cy.get(`ul>li`).eq(index).should('be.visible').contains(populationLevel);
     });
   });
 
   it('필터 선택', () => {
-    cy.get('div>svg>path').should('be.visible');
+    cy.get('div > svg > path').should('be.visible');
 
-    // 전체 해제
+    // 현재 마커 개수 세기
+    const 매우붐빔 = Cypress.$(
+      'div > svg[width="35"][height="50"] > path[fill="#FF1E1E"]'
+    ).length;
+    const 붐빔 = Cypress.$(
+      'div > svg[width="35"][height="50"] > path[fill="#FF9900"]'
+    ).length;
+    const 보통 = Cypress.$(
+      'div > svg[width="35"][height="50"] > path[fill="#FFDB1D"]'
+    ).length;
+    const 여유 = Cypress.$(
+      'div > svg[width="35"][height="50"] > path[fill="#43EB40"]'
+    ).length;
+
+    // 전체 필터링 해제
     populationLevels.map((_, index) => {
-      cy.get(`ul>li`).eq(index).click();
+      cy.get(`ul > li`).eq(index).click();
     });
+
     // 마커가 하나도 없어야 함
-    cy.get('div>svg').should('not.exist');
+    cy.get('div > svg').should('not.exist');
 
-    // 하나씩 눌렀을 때
-    populationColors.map((color, index) => {
-      cy.get(`ul>li`).eq(index).click();
-      const marker = cy.get('div>svg>path');
+    // 매우 붐빔 클릭
+    cy.get('ul > li').eq(0).click();
+    // 매우 붐빔 마커 개수 확인
+    cy.get('div > svg[width="35"][height="50"] > path[fill="#FF1E1E"]').should(
+      'have.length',
+      매우붐빔
+    );
+    // 매우 붐빔 클릭
+    cy.get('ul > li').eq(0).click();
 
-      if (marker) {
-        // 해당 컬러 존재
-        marker.should('have.attr', 'fill', color);
-        // 다른 컬러는 존재하지 않음
-        populationColors
-          .filter(other => other !== color)
-          .map(other => {
-            marker.should('not.have.attr', 'fill', other);
-          });
-      }
-      cy.get(`ul>li`).eq(index).click();
-    });
+    // 붐빔 클릭
+    cy.get('ul > li').eq(1).click();
+    // 붐빔 마커 개수 확인
+    cy.get('div > svg[width="35"][height="50"] > path[fill="#FF9900"]').should(
+      'have.length',
+      붐빔
+    );
+    // 붐빔 클릭
+    cy.get('ul > li').eq(1).click();
+
+    // 보통 클릭
+    cy.get('ul > li').eq(2).click();
+    // 보통 마커 개수 확인
+    cy.get('div > svg[width="35"][height="50"] > path[fill="#FFDB1D"]').should(
+      'have.length',
+      보통
+    );
+    // 보통 클릭
+    cy.get('ul > li').eq(2).click();
+
+    // 여유 클릭
+    cy.get('ul > li').eq(3).click();
+    // 여유 마커 개수 확인
+    cy.get('div > svg[width="35"][height="50"] > path[fill="#43EB40"]').should(
+      'have.length',
+      여유
+    );
+    // 여유 클릭
+    cy.get('ul > li').eq(3).click();
   });
 });
